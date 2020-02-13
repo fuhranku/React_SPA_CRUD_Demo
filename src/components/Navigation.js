bb8626ac1c09e166d35eaa87bf22ea97a5b0e9ef
Jose Tirado <jose_enrique_29@hotmail.com>
2020-02-12 20:18:36 -0400

    fixing merge

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -61,19 +61,22 @@ class Navigation extends Component{
     render(){
         return(
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">React-CRUD</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+                <a className="navbar-brand" href="#">React-CRUD</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    <NavItem path='/Create' name="Create"/>
-                    <NavItem path='/Read' name="Read"/>
-                    <NavItem path='/Update' name="Update"/>
-                    <NavItem path='/Delete' name="Delete"/>
-                </ul>
-            </div>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <NavItem path='/Create' name="Create"/>
+                        <NavItem path='/Read' name="Read"/>
+                        <NavItem path='/Update' name="Update"/>
+                        <NavItem path='/Delete' name="Delete"/>
+                    </ul>
+
+                    <button onClick={this.changeEnglish}>English</button>
+                    <button onClick={this.changeSpanish}>Spanish</button>
+                </div>
             </nav>
         );
     }
